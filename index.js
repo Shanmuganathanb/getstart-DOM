caeb9728c2d9151ad772129faab130e4093bdbac
Shanmuganathanb <38834254+Shanmuganathanb@users.noreply.github.com>
2021-03-13 14:17:53 +0530

    Creating Nodes and Modifying DOM

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ var headerTitle = document.getElementById('header-title');
 console.log(headerTitle.innerText);
 console.log(headerTitle.textContent);
 
- // changing the html content
-headerTitle.innerHTML = "<h3>Hello</h3>";
+
 // Change the style and use camelcase for style type
 headerTitle.style.borderBottom = "solid 3px #000";
 
@@ -47,12 +46,12 @@ secondItem.style.color='coral';
 var titles = document.querySelectorAll('.title');
 titles[0].textContent = "Hello";
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = "#f4f4f4";
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = "#f4f4f4";
 
-}
+// }
 
 var items = document.querySelectorAll('.list-group-item');
 items[2].style.backgroundColor = "green";
@@ -67,3 +66,48 @@ listItem[0].style.backgroundColor = "black";
 
 var listItemName = document.getElementsByTagName('li');
 listItemName[4].style.color = "white";
+
+listItemName[1].style.backgroundColor = "green";
+listItemName[2].style.display = "none";
+
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+	odd[i].style.backgroundColor = 'black';
+	odd[i].style.color="green";
+}
+
+// parentElement
+listItemName[2].parentElement.style.border ="thick solid #fff";
+// lastelementchild
+console.log(document.querySelector('ul').lastElementChild);
+document.querySelector('ul').lastElementChild.style.fontWeight ="bold";
+// lastchild
+console.log(document.querySelector('ul').lastChild);
+
+// appendchild,createElement,createTextNode
+var node = document.createElement("li");
+var textnode = document.createTextNode("Item 6");
+node.appendChild(textnode);
+document.querySelector("ul#items").appendChild(node);
+
+//firstElementChild and firstChild
+document.querySelector('ul').firstElementChild.style.color = "white";
+console.log(document.querySelector('ul').firstChild);
+
+// nextsibling and nextElementsibling
+console.log(document.querySelector('li.new-list-ele').nextSibling);
+document.querySelector('li.new-list-ele').nextElementSibling.innerText = "hello";
+
+// previoussibling and previousElementsibling
+console.log(document.querySelector('li.new-list-ele').previousSibling);
+document.querySelector('li.new-list-ele').previousElementSibling.innerText = "hi";
+
+// setAttribute
+document.querySelector("input[type='submit']").setAttribute("id","submit-btn");
+
+
+ // changing the html content
+//  headerTitle.parentElement = "<h3>Hello</h3>";
+headerTitle.insertAdjacentText("beforebegin", "HEllo");
+document.querySelector('li:nth-child(1)').insertAdjacentText("beforebegin","HEllo");
